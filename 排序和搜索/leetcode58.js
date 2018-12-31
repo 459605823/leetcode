741548e7df931b666e5363bdfb8110884c83f127
beast <459605823@qq.com>
2018-12-31 16:31:19 +0800

    good bye 2018

--- 排序和搜索/leetcode58.js
+++ 排序和搜索/leetcode58.js
@@ -7,6 +7,41 @@ function Interval(start, end) {
   this.end = end;
 }
 
+// 方法一：使用额外空间
 var merge = function(intervals) {
+  if(intervals.length == 0) return []
+  intervals.sort(function(a, b){
+    return a.start - b.start
+  })
+  var result = []
+  for(var i = 0; i < intervals.length - 1; i++){
+    if(intervals[i].end >= intervals[i + 1].start){
+      // 将下一个元素改为合并后的区间，在下一次循环时将其添加到结果数组中
+      intervals[i + 1].start = Math.min(intervals[i].start, intervals[i + 1].start)
+      intervals[i + 1].end = Math.max(intervals[i].end, intervals[i + 1].end)
+    } else {
+      result.push(intervals[i])
+    }
+  }
+  result.push(intervals[intervals.length - 1])
+  return result
+}
 
+// 方法二：在原数组上进行操作
+var merge = function(intervals) {
+  if(intervals.length == 0) return []
+  intervals.sort(function(a, b){
+    return a.start - b.start
+  })
+  var i = 0
+  for(var j = 1; j < intervals.length; j++){
+    if(intervals[j].start <= intervals[i].end){
+      intervals[i].end = Math.max(intervals[i].end, intervals[j].end)
+    } else {
+      // 从数组删去被合并的区间
+      intervals[++i] = intervals[j]
+    }
+  }
+  intervals.length = i + 1
+  return intervals
 }
